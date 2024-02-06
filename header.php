651f35e7d2a2cf56a231f10db6c746bf267917d9
dipeshs13 <dipeshshresthaa00@gmail.com>
2024-02-06 23:28:16 +0545

    Add Card and design Card

--- header.php
+++ header.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-  <header>
+  <header id="header">
     <nav id="nav_bar">
         <div class="logo">
             <img src="images/healthcare.png" alt="This is an logo" id="image1">
@@ -20,8 +21,8 @@
         <div class="center_nav">
             <div class="search">
                 <input type="text" id="text"/>
-                <button>
-                    Search
+                <button id="search_bar">
+                <i class="fa-solid fa-magnifying-glass"></i>
                 </button>
             </div>
         </div>
