ea8a622208e081ee83bc1fbb8b6965c20b3ceefa
dipeshs13 <dipeshshresthaa00@gmail.com>
2024-02-06 17:31:47 +0545

    First Commit

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <nav id="nav_bar">
+        <div class="logo">
+            <img src="images/healthcare.png" alt="This is an logo" id="image1">
+
+        </div>
+        <div class="h1">
+            <h1>Doctor Appointment System</h1>
+        </div>
+        
+        <div class="center_nav">
+            <div class="search">
+                <input type="text" id="text"/>
+                <button>
+                    Search
+                </button>
+            </div>
+        </div>
+        <div class="selection">
+            <label for="category">Category</label>
+            <select name="category" id="category_dropdown"
+              <option value="eye">Eye</option>
+              <option value="eye">Eye</option>
+              <option value="eye">Eye</option>
+              <option value="eye">Eye</option>
+            </select>
+        </div>
+       <div id="login">
+         <button>Login</button>
+       </div>
+       <div id="Register">
+         <button>Register</button>
+       </div>
+    </nav>
+  </header>
