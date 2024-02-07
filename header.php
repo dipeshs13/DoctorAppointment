719c6fe48d0cd4a831d3c1164000e3570e618bba
dipeshs13 <dipeshshresthaa00@gmail.com>
2024-02-07 10:08:30 +0545

    Design navbar

--- header.php
+++ header.php
@@ -6,6 +6,8 @@
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
 </head>
 <body>
   <header id="header">
@@ -26,7 +28,7 @@
                 </button>
             </div>
         </div>
-        <div class="selection">
+        <!-- <div class="selection">
             <label for="category">Category</label>
             <select name="category" id="category_dropdown"
               <option value="eye">Eye</option>
@@ -34,7 +36,20 @@
               <option value="eye">Eye</option>
               <option value="eye">Eye</option>
             </select>
-        </div>
+        </div> -->
+        <ul>
+          <li class="nav category">
+            <a href="#">Category
+            <span class="material-icons-outlined">
+              </span>
+            </a>
+            <ul class="drop-down">
+              <li><a href="#">Eye</a></li>
+              <li><a href="#">Eye</a></li>
+              <li><a href="#">Eye</a></li>
+            </ul>
+          </li>
+        </ul>
        <div class="button">
          <button id="login">Login</button>
        </div>
