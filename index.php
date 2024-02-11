49c8a5dfc36239a4c5d0eea1a99f9a4ced3f8d92
dipeshs13 <dipeshshresthaa00@gmail.com>
2024-02-12 01:16:10 +0545

    design card

--- index.php
+++ index.php
@@ -1,33 +1,5 @@
 <?php
 include "header.php";
-?>
-<div class="container">
-    <div class="card" id="card-1">
-      Card1
-    </div>
-    <div class="card" id="card-2">
-     Card2
-    </div>
-    <div class="card" id="card-3">
-     Card3
-    </div> 
-    <div class="card" id="card-4">
-      Card4
-    </div>
-    <div class="card" id="card-5">
-     Card5
-    </div>
-    <div class="card" id="card-6">
-       Card6
-    </div>
-    <div class="card" id="card-7">
-      Card7
-    </div>
-    <div class="card" id="card-8">
-    Card8
-    </div>
-    
-</div>
-<?php
+include "card.php";
 include "footer.php";
 ?>
